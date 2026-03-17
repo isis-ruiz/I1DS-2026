903743609c7c3e06768c8b100af31db02f2083f1
isis-ruiz <isisr5281@gmail.com>
2026-03-17 11:19:38 -0300

    Aprendendo sobre vetores

--- aulas/06-somandoTexto.js
+++ aulas/06-somandoTexto.js
@@ -3,7 +3,6 @@ let texto1 = "Estudar JavaScript ";
 let texto2 = "é muito legal!";
 let resultado = texto1 + texto2;
 
-
 console.log(resultado);
 resultado = resultado + " E nós vamos ficar bons nisso!";
 console.log(resultado);
