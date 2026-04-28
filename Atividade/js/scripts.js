7ef4eb732e5a8442c41c87edbbb6bc26fe16d71f
isis-ruiz <isisr5281@gmail.com>
2026-04-28 17:02:13 -0300

    Atividade HTML, JS e CSS

---
+++ Atividade/js/scripts.js
@@ -0,0 +1,44 @@
+const cadastrar = (event) => { 
+
+//Parar a propagacao padrao do evento *// 
+event.preventDefault(); 
+
+//Capturar os valores do elementos por Id*// 
+let nome = document.getElementById("nome").value; 
+let email = document.getElementById("email").value; 
+
+//Capturando o elemento de lista por Id*// 
+let id = document.getElementById("lista"); 
+
+//Criar um novo elemento <li>Nome - Email</li> 
+let novoItem = document.createElement("li");
+novoItem.innerHTML = 
+id + 
+" - " +
+nome +
+ " - " + 
+ email + 
+ " - " + 
+ "<p onClick=editar(" + 
+id + 
+") class= 'btn'>Editar</p>" + 
+"<p onClick=excluir(" + 
+id + 
+") class= 'btn'>Excluir</p>";
+//Adicionar o novo item na lista ja existente 
+lista.appendChild (novoItem); 
+//Limpar os campos 
+document.getElementById("nome").value = ""; 
+document.getElementById("email").value = ""; 
+}
+
+const excluir = (id) => {
+    const lista = document. getElementById("lista");
+    const itens = document.querySelectorAll("li"); 
+
+    itens.forEach((item) => {
+     if(item.innerHTML.includes(id)) {
+        item.remove(); 
+     }
+    }); 
+}; 
